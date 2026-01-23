41de9d017e7b872d1544042ec53d44be26d7be16
1024nettech <123078217+1024nettech@users.noreply.github.com>
2026-01-23 09:50:01 +0800

    Update link-verify.js

--- js/modules/link-verify.js
+++ js/modules/link-verify.js
@@ -1,1 +1,18 @@
+import { $ } from "./jquery.js";
+const url = location.href;
+let domain = location.origin;
+let domains = {
+    "https://www.qipeiyigou.com": "testpage",
+    "https://www.cheliangzulin.com": "clzl"
+};
 
+if (domain in domains) {
+    // 获取域名后面的部分
+    let path = url.replace(domain, "");
+
+    // 构建新的链接
+    let newUrl = `http://${domains[domain]}.qipeiyigou.com${path}`;
+
+    // 打开新链接
+    window.open(newUrl, "_blank");
+}
