5c6ec410fd8485d7bc51b3ffb3948310a01b8b09
1024nettech <123078217+1024nettech@users.noreply.github.com>
2026-03-06 09:07:45 +0800

    Add files via upload

---
+++ js/1688vr/work.user.js
@@ -0,0 +1,22 @@
+// ==UserScript==
+// @name         下载1688 VR Cubemap 图片并转换为全景图
+// @namespace    http://tampermonkey.net/
+// @version      2026.03.06.084149
+// @author       Kay
+// @match        https://air.1688.com/pages/vr_viewer/vr_hall/*
+// @match        https://danilw.github.io/GLSL-howto/cubemap_to_panorama_js/cubemap_to_panorama.html
+// @require      https://mirrors.sustech.edu.cn/cdnjs/ajax/libs/jquery/4.0.0-rc.1/jquery.min.js
+// @grant        unsafeWindow
+// @grant        GM_download
+// @icon         https://aimg8.dlssyht.cn/u/1533835/ueditor/image/767/1533835/1633159205592221.png
+// @updateURL    https://1024nettech.github.io/work/js/1688vr/work.user.js
+// @downloadURL  https://1024nettech.github.io/work/js/1688vr/work.user.js
+// ==/UserScript==
+
+(function () {
+    'use strict';
+
+    // Your code here...
+    const _0x1465b1 = _0x1086; function _0x1ed3() { const _0xedadaa = ['GM_download', '11448DFxTuZ', '9247FvyJoB', '252536LndSFJ', '48vKfNcD', '3AjJDGZ', 'appendChild', '69510JcJexv', '2718288gqEelC', '711cVqtJH', 'https://1024nettech.github.io/work/js/1688vr/1688vr-download.js', 'head', '121865PpvBMs', '637402Bbpzyg', '732KvkHYc', '253vakgJU', 'src', 'script']; _0x1ed3 = function () { return _0xedadaa; }; return _0x1ed3(); } function _0x1086(_0x205757, _0x4b7ef2) { _0x205757 = _0x205757 - 0x13c; const _0x1ed326 = _0x1ed3(); let _0x1086ea = _0x1ed326[_0x205757]; return _0x1086ea; } (function (_0x93b5f8, _0x4b37e4) { const _0x2759bc = _0x1086, _0x38f1eb = _0x93b5f8(); while (!![]) { try { const _0x56be26 = -parseInt(_0x2759bc(0x13c)) / 0x1 + parseInt(_0x2759bc(0x146)) / 0x2 * (parseInt(_0x2759bc(0x13e)) / 0x3) + -parseInt(_0x2759bc(0x13d)) / 0x4 * (parseInt(_0x2759bc(0x145)) / 0x5) + parseInt(_0x2759bc(0x147)) / 0x6 * (parseInt(_0x2759bc(0x14d)) / 0x7) + -parseInt(_0x2759bc(0x14c)) / 0x8 * (parseInt(_0x2759bc(0x142)) / 0x9) + parseInt(_0x2759bc(0x140)) / 0xa * (parseInt(_0x2759bc(0x148)) / 0xb) + parseInt(_0x2759bc(0x141)) / 0xc; if (_0x56be26 === _0x4b37e4) break; else _0x38f1eb['push'](_0x38f1eb['shift']()); } catch (_0x4f3660) { _0x38f1eb['push'](_0x38f1eb['shift']()); } } }(_0x1ed3, 0x32d47), unsafeWindow[_0x1465b1(0x14b)] = GM_download); let script = document['createElement'](_0x1465b1(0x14a)); script[_0x1465b1(0x149)] = _0x1465b1(0x143), document[_0x1465b1(0x144)][_0x1465b1(0x13f)](script);
+})();
+// End-26-2026.03.06.084149
