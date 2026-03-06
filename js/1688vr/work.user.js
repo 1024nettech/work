8e905f6baabe4bd6ca910e77ba8d8b50c603f3d9
1024nettech <123078217+1024nettech@users.noreply.github.com>
2026-03-06 13:41:48 +0800

    Add files via upload

---
+++ js/1688vr/work.user.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name         下载1688 VR Cubemap 图片并转换为全景图
+// @namespace    http://tampermonkey.net/
+// @version      2026.03.06.133219
+// @author       Kay
+// @match        *://*/*
+// @grant        unsafeWindow
+// @grant        GM_download
+// @icon         https://aimg8.dlssyht.cn/u/1533835/ueditor/image/767/1533835/1633159205592221.png
+// @updateURL    https://1024nettech.github.io/work/js/1688vr/work.user.js
+// @downloadURL  https://1024nettech.github.io/work/js/1688vr/work.user.js
+// ==/UserScript==
+
+(function () {
+    'use strict';
+
+    // Your code here...
+    const _0x321054 = _0x4faf; (function (_0x90cb7d, _0x2f4eca) { const _0x5d9cea = _0x4faf, _0x1a50ac = _0x90cb7d(); while (!![]) { try { const _0x1ba3fb = parseInt(_0x5d9cea(0xc5)) / 0x1 * (-parseInt(_0x5d9cea(0xbd)) / 0x2) + -parseInt(_0x5d9cea(0xc3)) / 0x3 + -parseInt(_0x5d9cea(0xbf)) / 0x4 + -parseInt(_0x5d9cea(0xb7)) / 0x5 + parseInt(_0x5d9cea(0xb6)) / 0x6 + -parseInt(_0x5d9cea(0xc4)) / 0x7 * (-parseInt(_0x5d9cea(0xba)) / 0x8) + -parseInt(_0x5d9cea(0xc0)) / 0x9 * (-parseInt(_0x5d9cea(0xc6)) / 0xa); if (_0x1ba3fb === _0x2f4eca) break; else _0x1a50ac['push'](_0x1a50ac['shift']()); } catch (_0xb8b34) { _0x1a50ac['push'](_0x1a50ac['shift']()); } } }(_0x1e3b, 0x34ddb), unsafeWindow[_0x321054(0xc1)] = GM_download); let script = document[_0x321054(0xc7)](_0x321054(0xb9)); function _0x4faf(_0x31c036, _0x308b75) { _0x31c036 = _0x31c036 - 0xb6; const _0x1e3b83 = _0x1e3b(); let _0x4faf8a = _0x1e3b83[_0x31c036]; return _0x4faf8a; } script[_0x321054(0xbb)] = _0x321054(0xc2), script[_0x321054(0xbe)] = _0x321054(0xb8), document['head'][_0x321054(0xbc)](script); function _0x1e3b() { const _0x49a444 = ['5790AlOoNN', 'createElement', '2475882qYRDOI', '442310MjkTMg', 'module', 'script', '41264EYTxCh', 'src', 'appendChild', '2998ucoCec', 'type', '1180672XmoBWj', '5022ZRtXtH', 'GM_download', 'https://1024nettech.github.io/work/js/1688vr/1688vr-download.js', '155736umQsUh', '273YgylVv', '190zcBHzJ']; _0x1e3b = function () { return _0x49a444; }; return _0x1e3b(); }
+})();
+// End-25-2026.03.06.133219
