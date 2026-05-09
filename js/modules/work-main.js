5f65b215466e0a24f54ee9b53725fa9294bbfd50
1024nettech <123078217+1024nettech@users.noreply.github.com>
2026-05-09 09:43:23 +0800

    Update work-main.js

--- js/modules/work-main.js
+++ js/modules/work-main.js
@@ -121,7 +121,7 @@ async function main() {
                         <span id="span1">查询中……</span><br>
                         <span id="span2">系统分类: </span><br>
                         <span id="span3">产品性质: </span><br>
-                        <span id="span4">专属车型: </span><br>
+                        <!-- <span id="span4">专属车型: </span><br> -->
                     </div>
                     <p id="tipx">正在检查中……</p>
                     `;
@@ -306,4 +306,4 @@ let interval = setInterval(function () {
         }
     }
 }, 1000);
-// End-309-2026.05.09.093912
+// End-309-2026.05.09.094305
